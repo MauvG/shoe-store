d08c454ee77ead3b39306c941d235235dcc1e758
MauvG <muaviaijazghazi@gmail.com>
2023-08-03 22:34:52 +0100

    started implemented shoe page

--- app/layout.js
+++ app/layout.js
@@ -18,7 +18,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={"min-h-screen flex flex-col"}>
+      <body className={roboto.className}>
         <Navbar />
         {children}
         <Footer />
--- components/ShoePage.jsx
+++ components/ShoePage.jsx
@@ -1,53 +1,59 @@
 import Image from "next/image";
 
 const ShoePage = ({ shoe }) => {
+  const sizes = [];
+  for (let index = 5; index <= 15; index += 0.5) {
+    sizes.push(index);
+  }
+
   return (
     <div>
-      <div className="flex justify-center text-center">
-        <div className="mt-20 flex-1 flex justify-center gap-4">
-          <div className="flex flex-col gap-2">
+      <div className="m-20 flex justify-center text-center">
+        <div className="flex-1 flex flex-col justify-end gap-4">
+          <div className="flex gap-2 justify-center">
             <Image
               src={"/" + shoe.name + "/main.png"}
               alt={shoe.name}
-              width={100}
-              height={100}
+              width={75}
+              height={75}
             />
             <Image
               src={"/" + shoe.name + "/alt.png"}
               alt={shoe.name}
-              width={100}
-              height={100}
+              width={75}
+              height={75}
             />
             <Image
               src={"/" + shoe.name + "/shoe.png"}
               alt={shoe.name}
-              width={100}
-              height={100}
+              width={75}
+              height={75}
             />
           </div>
           <div className="flex">
             <Image
               src={"/" + shoe.name + "/main.png"}
               alt={shoe.name}
-              width={500}
-              height={500}
+              width={750}
+              height={750}
+              className="object-contain"
             />
             {/* <Image
               src={"/" + shoe.name + "/alt.png"}
               alt={shoe.name}
-              width={500}
-              height={500}
+              width={750}
+              height={750}
             />
             <Image
               src={"/" + shoe.name + "/shoe.png"}
               alt={shoe.name}
-              width={500}
-              height={500}
+              width={750}
+              height={750}
             /> */}
           </div>
         </div>
 
-        <div className="flex-1 flex flex-col gap-2 mt-20">
+        <div className="flex-1 flex flex-col gap-2">
           <h1 className="text-2xl font-bold">{shoe.name}</h1>
           <div className="text-lg">
             {shoe.category === "men" ? <h1>Men's Shoes</h1> : <></>}
@@ -55,6 +61,21 @@ const ShoePage = ({ shoe }) => {
             {shoe.category === "kids" ? <h1>Kids' Shoes</h1> : <></>}
           </div>
           <h1 className="mt-4 text-lg">€{shoe.price}</h1>
+
+          <h1 className="mt-10 text-lg font-medium">Select Size</h1>
+          <div className="grid grid-cols-3 gap-2 ml-[10vw] mr-[10vw] text-lg">
+            {sizes.map((size) => (
+              <button className="border rounded-md p-3 shadow hover:border-black">
+                {size}
+              </button>
+            ))}
+          </div>
+
+          <div>
+            <button className="bg-black text-white rounded-full mt-4 p-4 pl-10 pr-10 shadow">
+              Add to Cart
+            </button>
+          </div>
         </div>
       </div>
     </div>
