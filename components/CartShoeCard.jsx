88de3d843127a0fb17f2c2dab4dccb92418f03a6
MauvG <muaviaijazghazi@gmail.com>
2023-09-01 09:09:17 +0100

    fixed data not refreshing on backend request

--- components/CartShoeCard.jsx
+++ components/CartShoeCard.jsx
@@ -1,7 +1,68 @@
+"use client";
+
 import { DeleteOutlineOutlined, KeyboardArrowDown } from "@mui/icons-material";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 const CartShoeCard = ({ shoe }) => {
+  const [sizes, setSizes] = useState([]);
+  const [dropwdown, setDrowdown] = useState(false);
+  const [drowdownClass, setDropdownClass] = useState(
+    "hidden absolute border bg-white ml-[4.5rem] z-10"
+  );
+
+  const removeShoe = async () => {
+    const id = shoe.id;
+    await fetch(`http://127.0.0.1:8090/api/collections/cart/records/${id}`, {
+      method: "DELETE",
+    });
+
+    reload();
+  };
+
+  const reload = () => {
+    let timer = setTimeout(() => {
+      window.location.reload();
+    }, 100);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  };
+
+  const initSizes = () => {
+    const array = [];
+    let count = 0;
+    for (let index = 5; index <= 15; index += 0.5) {
+      const size = {
+        index: count,
+        size: index,
+        chosen: false,
+      };
+      count++;
+      array.push(size);
+    }
+    return array;
+  };
+
+  useEffect(() => {
+    setSizes(initSizes());
+  }, []);
+
+  const showDropdown = () => {
+    if (dropwdown == false) {
+      setDrowdown(true);
+      setDropdownClass(
+        "flex flex-col absolute border bg-white ml-[4.5rem] z-10"
+      );
+    } else {
+      setDrowdown(false);
+      setDropdownClass("hidden absolute border bg-white ml-[4.5rem] z-10");
+    }
+  };
+
+  const handleSizeChange = () => {};
+
   return (
     <div id="CartShoeCard" className="flex">
       <div>
@@ -24,18 +85,22 @@ const CartShoeCard = ({ shoe }) => {
         <div className="flex gap-4">
           <h1 className="">
             Size UK {shoe.size + " "}
-            <button>
+            <button onClick={showDropdown}>
               <KeyboardArrowDown />
             </button>
-          </h1>
-          <h1 className="">
-            Quantity {shoe.quantity + " "}
-            <button>
-              <KeyboardArrowDown />
-            </button>
+            <div className={drowdownClass}>
+              {sizes.map((size) => (
+                <button
+                  onClick={handleSizeChange}
+                  className="text-left text-sm leading-4 pl-2 pr-2 hover:bg-slate-800 hover:text-white"
+                >
+                  {size.size}
+                </button>
+              ))}
+            </div>
           </h1>
         </div>
-        <button className="text-left mt-8">
+        <button onClick={removeShoe} className="text-left mt-8">
           <DeleteOutlineOutlined />
         </button>
       </div>
