2b5097e25fec6b52b2c42deec572d000e044c02b
MauvG <muaviaijazghazi@gmail.com>
2023-09-06 20:04:43 +0100

    forgot to add alt tag

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Loading = () => {
   return (
     <div className="m-auto h-[50%]">
-      <Image src="/loading.gif" width={100} height={100} />
+      <Image src="/loading.gif" width={100} height={100} alt="loading" />
     </div>
   );
 };
